6103b3816f4757042556a0893b4c5d95fd45988b
Derick <derickchoskinson@gmail.com>
2024-11-13 18:54:06 -0600

    remove purple border

--- components/shared/nav-bar.tsx
+++ components/shared/nav-bar.tsx
@@ -18,12 +18,12 @@ const NavigationBar = async () => {
   const userData = await getUser();
 
   return (
-    <nav className="fixed left-0 right-0 top-0 z-50 flex items-center justify-between border-2 border-purple-500 bg-background/80 px-4 shadow-md backdrop-blur-sm transition-transform duration-300 ease-in-out sm:px-6 lg:px-8">
+    <nav className="fixed left-0 right-0 top-0 z-50 flex items-center justify-between bg-background/80 px-4 shadow-md backdrop-blur-sm transition-transform duration-300 ease-in-out sm:px-6 lg:px-8">
       <NavLinks />
       <NavigationPath />
       <div className="flex items-center space-x-4">
-        {userData !== null && (
-          <DropdownMenu modal={false}>
+        { userData !== null && (
+          <DropdownMenu modal={ false }>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                 <Avatar className="h-8 w-8">
@@ -31,23 +31,23 @@ const NavigationBar = async () => {
                     className="object-cover"
                     src={
                       userData.userImages.length > 0 &&
-                      userData.userImages[0].userAvatar === true
+                        userData.userImages[0].userAvatar === true
                         ? userData.userImages[0].imageUrl
                         : "/assets/images/placeholder-user.jpg"
                     }
-                    alt={userData.name || "User avatar"}
+                    alt={ userData.name || "User avatar" }
                   />
                   <AvatarFallback>
-                    {userData.name ? (
+                    { userData.name ? (
                       userData.name.charAt(0).toUpperCase()
                     ) : (
                       <User />
-                    )}
+                    ) }
                   </AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent avoidCollisions={true}>
+            <DropdownMenuContent avoidCollisions={ true }>
               <DropdownMenuItem>
                 <Link href="/profile">Profile</Link>
               </DropdownMenuItem>
@@ -59,11 +59,11 @@ const NavigationBar = async () => {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        )}
+        ) }
 
-        {!userData && <Link href="/login">Login</Link>}
-        {userData === null && <ModeToggle />}
-      </div>{" "}
+        { !userData && <Link href="/login">Login</Link> }
+        { userData === null && <ModeToggle /> }
+      </div>{ " " }
     </nav>
   );
 };
