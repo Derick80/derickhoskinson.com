b3708624e58013278b8ac2b373eff60f0ea79e45
Derick <derickchoskinson@gmail.com>
2024-11-16 00:11:01 -0600

    updated componets

--- components/mdx/sync-functions.tsx
+++ components/mdx/sync-functions.tsx
@@ -1,11 +1,5 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from 'react'
 
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import rehypeStringify from 'rehype-stringify'
-import rehypePrettyCode from 'rehype-pretty-code'
-import { transformerCopyButton } from '@rehype-pretty/transformers'
 export const MDXPre = (
     MDXPreProps: DetailedHTMLProps<
         HTMLAttributes<HTMLPreElement>,
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
-}
-
-export function copyToClipboard(text: string) {
-    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
-        return navigator.clipboard.writeText(text)
-    return Promise.reject('The Clipboard API is not available.')
+  return twMerge(clsx(inputs))
 }
